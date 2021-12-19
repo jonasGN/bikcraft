2d0e0d919808ba8a13ecc725809b45dc919bf0b5
jonasGN <j.goncalvesneto@outlook.com>
2021-12-19 16:25:35 -0300

    some scripts add

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+// active header indication in current page
+const primaryNavigation = document.querySelectorAll(".header-menu a");
+
+function activeLink(item) {
+  const currentUrl = window.location.href;
+  const currentTab = item.href;
+
+  if (currentUrl.includes(currentTab)) {
+    item.classList.add("active");
+  }
+}
+
+if (primaryNavigation) primaryNavigation.forEach(activeLink);
+
+// active insurance items
+const insuranceParams = new URLSearchParams(window.location.search);
+
+function activeProduct(param) {
+  const element = document.getElementById(param);
+  if (element) element.checked = true;
+}
+
+if (insuranceParams) insuranceParams.forEach(activeProduct);
+
+// insurance questions expand control
+const questions = document.querySelectorAll(".insurance-questions-list button");
+
+function expandQuestions(question) {
+  question.addEventListener("click", (event) => {
+    const currentElement = event.currentTarget;
+
+    const controls = currentElement.getAttribute("aria-controls");
+    const anwser = document.getElementById(controls);
+
+    const isActive = anwser.classList.toggle("active");
+    console.log(isActive);
+    currentElement.setAttribute("aria-expanded", isActive);
+  });
+}
+
+if (questions) questions.forEach(expandQuestions);
+
+// bike image galery control
